b4dd3886bbb6f33ee66f77d0f0952618c9d46d48
Lesha-spr <voodoo.spr@gmail.com>
2015-09-10 23:40:59 +0300

    MM-0000: added basic structure.

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+var minifyCSS = require('gulp-minify-css');
+var inject = require('gulp-inject');
+var less = require('gulp-less');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var uglify = require('gulp-uglify');
+var watch = require('gulp-watch');
+
+gulp.task('templates', function() {
+    var LOCALS = {};
+
+    gulp.src('./src/templates/pages/**/*.jade')
+        .pipe(jade({
+            locals: LOCALS,
+            pretty: true
+        }))
+        .pipe(gulp.dest('./build/templates/pages/'));
+});
+
+gulp.task('less', function() {
+    return gulp.src(['src/styles/styles.less'])
+    /**
+     * Dynamically injects @import statements into the main app.less file, allowing
+     * .less files to be placed around the app structure with the component
+     * or page they apply to.
+     */
+        .pipe(inject(gulp.src(['./components/**/*.less'], {read: false, cwd: 'src/styles/'}), {
+            starttag: '/* inject:imports */',
+            endtag: '/* endinject */',
+            transform: function (filepath) {
+                return '@import ".' + filepath + '";';
+            }
+        }))
+        .pipe(less())
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('build/styles'));
+});
+
+gulp.task('compress', function() {
+    var b = browserify({
+        entries: 'src/js/app.js',
+        debug: true,
+        // defining transforms here will avoid crashing your stream
+        transform: [babelify]
+    });
+
+    return b.bundle()
+        .pipe(source('./app.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+        .on('error', gutil.log)
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('watch', function() {
+    watch('./src/js/**/*', function() {
+        gulp.run(['compress']);
+    });
+
+    watch('./src/styles/**/*', function() {
+        gulp.run(['less']);
+    });
+
+    watch('./src/templates/**/*', function() {
+        gulp.run(['templates']);
+    });
+});
+
+gulp.task('default', ['templates', 'compress', 'less', 'watch']);
