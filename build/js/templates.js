56d9467db2d5618f9e092a23e91d27b4f1faa8a9
Oleksii An <Oleksii_An@epam.com>
2015-09-18 12:03:11 +0300

    MM-TECH: publish on gh-pages.

---
+++ build/js/templates.js
@@ -0,0 +1,5 @@
+this["MM"] = this["MM"] || {};
+this["MM"]["templates"] = this["MM"]["templates"] || {};
+this["MM"]["templates"]["component"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<div></div>";
+},"useData":true});
