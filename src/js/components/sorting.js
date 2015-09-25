7e3847b4215c985c5460cfb5f55f4c5d6fd9deff
Lesha-spr <voodoo.spr@gmail.com>
2015-09-25 13:30:00 +0300

    MM-0017: working on sorting component.

--- src/js/components/sorting.js
+++ src/js/components/sorting.js
@@ -1,5 +1,16 @@
 import $ from 'jquery';
 import app from './../app.js';
+import state from './../helpers/state.js';
+
+const DEFAULTS = {
+    SELECTORS: {
+        ITEM: '.sorting__item'
+    },
+    CLASSES: {
+        ACTIVE: 'sorting__item_state_active'
+    },
+    SORTING: 'rating'
+};
 
 class Sorting {
     constructor(element) {
@@ -7,6 +18,7 @@ class Sorting {
 
         this.elems = {
             $root: $root,
+            $item: $root.find(DEFAULTS.SELECTORS.ITEM),
             $window: $(window),
             $body: $(document.body)
         };
@@ -16,10 +28,31 @@ class Sorting {
     }
 
     initialize() {
-
+        this.setActive();
     }
 
     bindEvents() {
+        this.elems.$root.on('click', DEFAULTS.SELECTORS.ITEM, this.handleSorting.bind(this));
+    }
+
+    setActive() {
+        let sorting = state.getState().sorting || DEFAULTS.SORTING;
+        console.log(sorting);
+    }
+
+    handleSorting(event) {
+        let $current = $(event.currentTarget);
+        let sorting = $current.data('sorting');
+
+        event.preventDefault();
+
+        this.elems.$item.removeClass(DEFAULTS.CLASSES.ACTIVE);
+        $current.addClass(DEFAULTS.CLASSES.ACTIVE);
+
+        this.sort(sorting);
+    }
+
+    sort(sorting) {
 
     }
 }
