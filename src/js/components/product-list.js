b74a13544b9c22ed8569c1cb093d4bfa0c732a7b
Oleksii An <Oleksii_An@epam.com>
2015-10-02 16:48:52 +0300

    MM-0021: added styles for loading products.

--- src/js/components/product-list.js
+++ src/js/components/product-list.js
@@ -3,6 +3,16 @@ import _ from 'lodash';
 import app from './../app.js';
 import AjaxError from './../errors/base.js';
 
+const DEFAULTS = {
+    SELECTORS: {
+        ITEM: '.product-list__item',
+        IMAGE: '.product-list__item__img'
+    },
+    CLASSES: {
+        ITEM_LOADING: 'product-list__item_state_loading'
+    }
+};
+
 class ProductList {
     constructor(element) {
         let $root = $(element);
@@ -36,9 +46,17 @@ class ProductList {
         });
     }
 
+    rotateItem(event) {
+        let $current = $(event.currentTarget);
+
+        $current.parents(DEFAULTS.SELECTORS.ITEM).removeClass(DEFAULTS.CLASSES.ITEM_LOADING);
+    }
+
     render(data) {
         let template = app.templates['product-list'](data);
         this.elems.$root.html(template);
+
+        this.elems.$root.find(DEFAULTS.SELECTORS.IMAGE).one('load', this.rotateItem.bind(this));
     }
 }
 
