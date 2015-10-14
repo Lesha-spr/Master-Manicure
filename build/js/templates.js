a4d371dc6441717690ef316984f8a1c50e93d6e0
Lesha-spr <voodoo.spr@gmail.com>
2015-10-14 10:53:11 +0300

    MM-0000: recompile.

--- build/js/templates.js
+++
@@ -1,5 +0,0 @@
-this["MM"] = this["MM"] || {};
-this["MM"]["templates"] = this["MM"]["templates"] || {};
-this["MM"]["templates"]["component"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div></div>";
-},"useData":true});
