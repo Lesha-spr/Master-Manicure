40233447dd7cb99fa6343cbb1a09b85d5b89e65b
Lesha-spr <voodoo.spr@gmail.com>
2015-11-14 00:39:08 +0200

    MM-TECH: updated build.

--- src/js/components/spinner.js
+++ src/js/components/spinner.js
@@ -4,21 +4,21 @@ import $ from 'jquery';
 const DEFAULTS = {
     SELECTORS: {
         SPINNER: '.spinner'
+    },
+    CLASSES: {
+        FIXED: 'spinner_fixed'
     }
 };
 
 class Spinner {
     constructor() {
-        this.template = null;
-        this.initialize();
+        this.spinner = null;
     }
 
-    initialize() {
-        this.template = app.templates['spinner']();
-    }
+    show($element, isFixed) {
+        this.spinner = app.templates['spinner']({isFixed: isFixed});
 
-    show($element) {
-        $element.append(this.template);
+        $element.append(this.spinner);
     }
 
     hide($element) {
